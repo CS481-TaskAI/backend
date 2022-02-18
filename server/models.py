027dbf1c5cd9623cb68d9953806952134af670ad
Cesar <ceezy94@gmail.com>
2022-02-17 20:21:00 -0800

    Added functions to represent a record as json string

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from . import db
 from datetime import datetime
+import json
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -8,11 +9,19 @@ class User(db.Model):
     email = db.Column(db.String(30), unique = True)
     password = db.Column(db.String(30))
 
+    def __repr__(self):
+        dictionary = {'id':self.id, 'username':self.username, 'email':self.email}
+        return json.dumps(dictionary, indent = 3)
+
 class Project(db.Model):
     __tablename__ = 'project'
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(20))
     description = db.Column(db.String(120))
+
+    def __repr__(self):
+        dictionary = {'id':self.id, 'title':self.title, 'description':self.description}
+        return json.dumps(dictionary, indent = 3)
 
 class Task(db.Model):
     __tablename__ = 'task'
@@ -26,6 +35,13 @@ class Task(db.Model):
     classification = db.Column(db.String(10))
     timing = db.Column(db.String(10)) #Not what type timing should be
     status = db.Column(db.Boolean, default = False) #False = not completed
+
+    def __repr__(self):
+        dictionary = {'id':self.id, 'description':self.description, 'assigned':str(self.date_assigned),
+                       'due':str(self.date_due), 'priority':self.priority, 'classification':self.classification,
+                        'timing':self.timing, 'status':self.status}
+        return json.dumps(dictionary, indent = 3)
+                
 
 
 class UserProjectLink(db.Model):
