c8483542e18e7880b1697dc7c5d0c8837cf1864c
Cesar <ceezy94@gmail.com>
2022-02-17 20:19:28 -0800

    added getUserTasks, rewrote getUserProjects with a join - now returns all attributes of each project

--- pullRecords.py
+++ pullRecords.py
@@ -3,15 +3,22 @@ from server import db, create_app
 from datetime import date
 import MySQLdb
 
-#TODO: functions currently print the query, do something more useful with it
-
 app = create_app()
 
 # get all user projects given user id
 def getUserProjects(u_id):
     with app.app_context():
-        print(UserProjectLink.query.with_entities(UserProjectLink.project_id).filter_by(user_id=u_id).all())
+        records = db.session.query(Project).join(UserProjectLink).filter(UserProjectLink.user_id==u_id)
+        listed = records.all()
+        return listed
 
 # get all of a projects users, given project id
 # def getProjectUsers(p_id):
-    
+
+
+# gets all tasks associated with a user
+def getUserTasks(u_id):
+    with app.app_context():
+        records = db.session.query(Task).filter_by(user_id=u_id)
+        listed = records.all()
+        return listed
