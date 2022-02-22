06affee3c0f8714a6bca8b0f744332a899e3b124
Cesar <ceezy94@gmail.com>
2022-02-21 18:06:32 -0800

    update some imports

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #Connects with mysql container and creates tables
 
 #SQLAlchemy
-from server.models import *
-from server import db, create_app
+from server.flask_app.models import *
+from server.flask_app.app import db, create_app
 db.create_all(app=create_app())
     
