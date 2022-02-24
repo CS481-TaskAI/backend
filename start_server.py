88b95e2c5d1e29d4277a15b95978ded0474cbbed
Cesar <ceezy94@gmail.com>
2022-02-23 20:01:11 -0800

    Created endpoints for connection, some utility

--- start_server.py
+++ start_server.py
@@ -11,35 +11,73 @@ get = Get(app)
 add = Add(app)
 delete = Delete(app)
 
-#This the is route to the basic url
-#Demo will print
-@app.route('/')
-def index():
-    
-    resp = get.getUserTasks(2)
-    answer = resp[0].to_str()
-    print(answer)
+#--------------------Get and Post routes------------------------------------------
 
-    
-    
-    return answer
-
-# Once the user has been authenticated they will be redirected
-# to this function with their username and id
-@app.route('/home', methods = ['GET', 'POST'])
-def homePage():
+# Will take the username and password and authenticate
+# Returns user id
+# Post will attempt to create database records if email or username don't exist
+@app.route('/user', methods = ['GET', 'POST'])
+def login_signup():
     #here we will pass take the id from the request and pull 
     #the records using the id
     pass
 
-# The users project page, here they will need all of their tasks
-# regarding the project.
-# They can create new tasks
+# Returns all projects given a user_id
+# Creates project if all parameters valid, returns projects
 @app.route('/projects', methods = ['GET', 'POST'])
-def projectsPage():
+def projects():
+    pass
+
+#Returns all tasks given a user_id
+#Creates task if all parameters valid, returns tasks
+@app.route('/tasks', methods = ['GET', 'POST'])
+def tasks():
+    
+    if request.method == 'POST':
+        u_id = request.form["user_id"]
+        # rest of the attributes
+        # then,
+        # add users with these parameters
+
+    obs = get.getUserTasks(1)
+    list_of_dicts = to_list_dict(obs)
+    return jsonify(list_of_dicts)
+
+#Returns all contacts given user_id
+#creates contact given contact username
+@app.route("/contact", methods = ['GET', 'POST'])
+def contacts():
+    # if either, after reroute to GET /c
+    pass
+
+#--------------------Update and Delete routes---------------------------------------
+
+#Modify or delete Projects, reroutes to associated Get route
+@app.route('/mod_projects', methods = ['PUT', 'DELETE'])
+def update_projects():
+    # if either, afterwards reroute to GET /projects
+    pass
+
+#Modify or delete Tasks, reroutes to associated Get route
+@app.route('/mod_tasks', methods = ['PUT', 'DELETE'])
+def update_tasks():
+    # if either, afterwards reroute to GET /tasks
+    pass
+
+#Modify or delete Contacts, reroutes to associated Get route
+@app.route("/mod_contact", methods = ['PUT', 'DELETE'])
+def update_contacts():
     pass
 
 
+#-------------------Utility--------------------------------------------------
+# Takes list of db records and converts to list of dictionaries
+# returns list of dictionaries
+def to_list_dict(obs):
+    my_list = []
+    for rec in obs:
+        my_list.append(rec.as_dict())
+    return my_list
 
 if __name__ == '__main__':
 
