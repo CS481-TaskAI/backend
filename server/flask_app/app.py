97582f9db1503da6107a61563f91ba1052193cc8
trisolaris3 <sj191999@gmail.com>
2022-03-05 10:58:00 -0800

    updated connection string for RDS
    ... and commented out the old one

--- server/flask_app/app.py
+++ server/flask_app/app.py
@@ -6,7 +6,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
                                             #URI to connect to db -- database must exist before
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:my-secret-pw@localhost:3306/taskai"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://admin:secretpw@taskai-db.cumsrf0e0mue.us-west-2.rds.amazonaws.com:3306/taskai"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:my-secret-pw@localhost:3306/taskai"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
